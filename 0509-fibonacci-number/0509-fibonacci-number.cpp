7cf1e4ef5799dd1fe1c780ef7aaf1a0a0d3ad7ea
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-02-05 11:42:07 +0530

    Time: 13 ms (31.57%), Space: 7.8 MB (36.17%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    //recursive approach
+    int fib(int n) {
+        if(n<=1)
+        {
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+};
