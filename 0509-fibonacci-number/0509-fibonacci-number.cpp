6ac98ee6f38a6bf1279162d262e4a11733ab6663
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-02-05 11:46:26 +0530

    Time: 3 ms (64.64%), Space: 8.2 MB (15.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    //recursive approach
-    int fib(int n) {
+    //Top down Approach memoization
+    int fibon(int n,vector<int>&dp)
+    {
         if(n<=1)
         {
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1)
+        return dp[n];
+        return dp[n]=fibon(n-1,dp)+fibon(n-2,dp);
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return fibon(n,dp);
     }
 };
