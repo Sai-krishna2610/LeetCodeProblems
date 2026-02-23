bc0c93d4c2082714eed57b9e210418e65ede41d8
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-02-23 16:08:27 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,53 +1,26 @@
 class Solution {
 public:
-    int findPos(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1, mid;
-        int ans = nums.size();
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (mid != nums.size() - 1) {
-                if (nums[mid] < target && nums[mid + 1] > target) {
-                    return mid + 1;
-                }
-                else
-                {
-                    if (nums[mid] < target) {
-                    end = mid - 1;
-                } else {
-                    ans=mid+1;
-                    start = mid + 1;
-                }
-                }
-            } else {
-                if (nums[mid] < target) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        int ans=nums.size();
+
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                ans=mid;
+                right=mid-1;
             }
         }
         return ans;
-    }
-
-    int searchInsert(vector<int>& nums, int target) {
-        int start = 0, end = nums.size() - 1, mid;
-        int ans = -1;
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                return ans;
-            } else if (nums[mid] > target) {
-                ans=mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        if (ans == -1) {
-            return findPos(nums, target);
-        } else {
-            return ans;
-        }
     }
 };
