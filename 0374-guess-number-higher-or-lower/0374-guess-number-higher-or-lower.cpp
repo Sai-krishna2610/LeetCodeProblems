81f21d1d95a4e3dddf002483d9ece4c9a2236b13
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-02-23 18:26:52 +0530

    Time: 2 ms (56.84%), Space: 8 MB (41.9%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -0,0 +1,39 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+/*
+This problem is similar searching an element using binary search 
+but instead of checking mid is equal to target 
+here we are using guess function 
+*/
+class Solution {
+public:
+    int guessNumber(int n) {
+        long left = 1;
+        long right = n;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+
+            int result = guess(mid);
+
+            if (result == 0) {
+                return mid;        
+            }
+            else if (result == -1) {
+                right = mid - 1;  
+            }
+            else {
+                left = mid + 1;   
+            }
+        }
+
+        return -1;
+    }
+};
