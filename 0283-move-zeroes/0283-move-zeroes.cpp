94b69dfed23d8d4dae25c54ff5262753e9bc5717
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-05-08 11:10:26 +0530

    Time: 2 ms (15.64%), Space: 23.8 MB (81.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;  
+        int i = 0;  // i points position to place next non zero element
 
-        for (int j = 0; j < nums.size(); j++) {
+        for (int j = 0; j < nums.size(); j++) { 
             if (nums[j] != 0) {
                 swap(nums[i], nums[j]);
                 i++;
