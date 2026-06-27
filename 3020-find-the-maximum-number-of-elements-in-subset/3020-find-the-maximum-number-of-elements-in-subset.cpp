d9b2e54dc7177e193b787be3ebb9bd4287302cc8
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-06-27 21:19:29 +0530

    Time: 69 ms (78.29%), Space: 122.6 MB (52%) - LeetHub

--- 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -6,7 +6,6 @@ public:
 
         int ans = 1;
 
-        // Handle value 1 separately
         if (cnt.count(1))
             ans = max(ans, cnt[1] - (cnt[1] % 2 == 0));
 
@@ -18,16 +17,15 @@ public:
 
             while (true) {
                 if (!cnt.count(cur) || cnt[cur] == 0) {
-                    if (len > 0) len--;   // previous level becomes center
+                    if (len > 0) len--;   
                     break;
                 }
 
                 if (cnt[cur] == 1) {
-                    len++;               // use as center
+                    len++;               
                     break;
                 }
 
-                // cnt[cur] >= 2
                 len += 2;
 
                 if (cur > 1000000000LL) break;
