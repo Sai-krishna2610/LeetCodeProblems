ba5471f30efac7f56d65fb374994d66185acc73a
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-08-22 22:56:17 +0530

    Time: 0 ms (100%), Space: 10.9 MB (38.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    //optimized approach
-    int robLinear(vector<int>& nums, int start, int end) {
-        int prev2 = 0;  // dp[i-2]
-        int prev1 = 0;  // dp[i-1]
-
-        for (int i = start; i <= end; i++) {
-            int curr = max(prev1, nums[i] + prev2);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
+    int solve(vector<int>& nums, int i, int end, vector<int>& dp) {
+        if (i > end) return 0;
+        if (dp[i] != -1) return dp[i];
+        int rob = nums[i] + solve(nums, i + 2, end, dp);
+        int skip = solve(nums, i + 1, end, dp);
+        return dp[i] = max(rob, skip);
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
         if (n == 1) return nums[0];
-
-        return max(robLinear(nums, 0, n - 2),robLinear(nums, 1, n - 1)
-        );
+        vector<int> dp1(n, -1), dp2(n, -1);
+        return max(solve(nums, 0, n - 2, dp1),solve(nums, 1, n - 1, dp2));
     }
 };
