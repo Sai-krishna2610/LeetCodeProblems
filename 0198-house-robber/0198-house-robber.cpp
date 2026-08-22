dc84826af9b93ffdceb1536e1e65eb004572d3d5
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-08-22 22:53:15 +0530

    Time: 0 ms (100%), Space: 10.5 MB (71.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    //Optimized Approach
-    int rob(vector<int>& nums) {
-        if (nums.size()==0)
-        return 0;
-        if (nums.size()==1)
-        return nums[0];
-        if (nums.size()==2)
-        return max(nums[0],nums[1]);
-        int firstPart=nums[0];
-        int secondPart=max(nums[0],nums[1]);
-        int ans=0;
-        for(int i=2;i<nums.size();i++)
+    int robber(vector<int>&nums,int index,vector<int>&dp)
+    {
+        if(index==0)
         {
-            ans=max(nums[i]+firstPart,secondPart);
-            firstPart=secondPart;
-            secondPart=ans;
+            return nums[0];
         }
-        return ans;
+        if(index==1)
+        {
+            return max(nums[0],nums[1]);
+        }
+        if (dp[index]!=-1)
+        return dp[index];
+        return dp[index]=max(nums[index]+robber(nums,index-2,dp),robber(nums,index-1,dp));
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size()+1,-1);
+        return robber(nums,nums.size()-1,dp);
     }
 };
