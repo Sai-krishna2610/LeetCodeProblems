a27d35c4731aaea7eaa0adebe82e95096b53e393
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-08-22 05:56:32 +0530

    Time: 0 ms (100%), Space: 17.7 MB (53.68%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 class Solution {
 public:
-//optimized approach
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
+    // Top down approach memoization
+    int ways(int n, vector<int>& cost, vector<int>& dp) {
         if (n <= 1) 
         return 0;
-        int prev2 = 0;
-        int prev1 = 0;
-        for (int i = 2; i <= n; i++) {
-            int curr = min(cost[i - 1] + prev1,
-                           cost[i - 2] + prev2);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        if (dp[n] != -1) 
+        return dp[n];
+        return dp[n] = min(cost[n - 1] + ways(n - 1, cost, dp),
+                    cost[n - 2] + ways(n - 2, cost, dp));
+    }
+
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1, -1); 
+        return ways(n, cost, dp);
     }
 };
