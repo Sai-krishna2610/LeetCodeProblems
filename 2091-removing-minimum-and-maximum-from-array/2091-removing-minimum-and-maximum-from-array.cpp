026d5596aef64baeeadd721ab066fdb3f7062bbf
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-08-30 15:58:57 +0530

    Time: 0 ms (100%), Space: 92.1 MB (22.14%) - LeetHub

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n=nums.size();
+
+        int minIndex=0,maxIndex=0;
+
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]<nums[minIndex])
+            {
+                minIndex=i;
+            }
+            if(nums[i]>nums[maxIndex])
+            {
+                maxIndex=i;
+            }
+        }
+        int left=min(minIndex,maxIndex);
+        int right=max(minIndex,maxIndex);
+
+        int fromFront=right+1;
+        int fromBack=n-left;
+        int bothSides=(left+1)+(n-right);//Here we can do without paranthesis but if we use parenthsis we can understand easily 
+        return min({fromFront,fromBack,bothSides});
+    }
+};
