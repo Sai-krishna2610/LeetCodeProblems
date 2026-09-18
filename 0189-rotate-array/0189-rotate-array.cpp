9f469f2606228ac8204642409386772e4977cb9c
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-09-18 19:38:52 +0530

    Time: 0 ms (100%), Space: 263.4 MB (37.68%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -10,7 +10,7 @@ public:
         }
     }
     void rotate(vector<int>& nums, int k) {
-        k=k%nums.size();
+        k = k % nums.size();
         reverse(nums,0,nums.size()-1);
         reverse(nums,0,k-1);
         reverse(nums,k,nums.size()-1);
