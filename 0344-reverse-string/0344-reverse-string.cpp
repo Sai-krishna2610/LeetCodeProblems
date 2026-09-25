5612db1e5bc12d2e7eeb635671e80f437502da50
s sai krishna <91758593+Sai-krishna2610@users.noreply.github.com>
2026-09-25 22:33:33 +0530

    Time: 0 ms (100%), Space: 27.1 MB (80.64%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-    stack<char>st;
-	for(int i=0;i<s.size();i++){
-		st.push(s[i]);
-	}
-	int i=0;
-	while(!st.empty())//st.empty function returns 1 if stack empty
-	//returns 0 when stack contains elements
-	
-	{
-		s[i]=st.top();//returns top element into stack
-		i++;
-		st.pop();
-	}
+        int left=0,right=s.size()-1;
+        while(left<right)
+        {
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        }
     }
 };
